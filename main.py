06fa8a937d1146735839e69271f482706ba7a1b0
Vili Sinervä <vili.m.sinerva@gmail.com>
2022-07-27 17:46:39 +0300

    Improve data entry

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
+from os import system, name
+def clear():
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+
+    # for mac and linux
+    else:
+        _ = system('clear')
+
 class Calculator:
     def __init__(self):
+        self._max_precision = 10**5
+        self.zero_values()
+
+    def zero_values(self):
+        self._target_entered = False
         self._target_coords = (0,0)
+        self._target_pos = (0,0)
+
+        self._observer_entered = False
         self._observer_coords = (0,0)
+        self._observer_pos = (0,0)
+
+        self._firing_position_entered = False
         self._firing_position_coords = (0,0)
+        self._firing_position = (0,0)
+
+        self._observer_az = 0
+        self._observer_dist_to_target = 0
 
     def _check_exit(self, string):
         if string.upper() == "E":
@@ -22,10 +47,9 @@ class Calculator:
                 northing_str = string[str_len//2:str_len]
                 northing = int(northing_str)
 
-                if easting < 0 or northing < 0:
-                    raise ValueError
-
-                return (easting, northing)
+                if 0 <= easting < self._max_precision and 0<= northing < self._max_precision:
+                    return (easting, northing)
+                raise ValueError
             else:
                 raise ValueError
         except ValueError:
@@ -38,37 +62,48 @@ class Calculator:
     def _get_coordinates(self, pos_name: str):
         coords = None
         while not coords:
-            coords_str = input(f"{pos_name} coordinates ('E' to Exit): ")
-            self._check_exit(coords_str)
+            coords_str = input(f"{pos_name} coordinates: ")
             coords = self._str_to_coords(coords_str)
         return coords
 
     def get_starting_values(self):
+        self.zero_values()
         action_str = "_"
+        confirm_str = "DATA ENTERED"
         while action_str != "":
-            confirm_str = "DATA ENTERED"
-            print()
+            clear()
             print(confirm_str)
             print("-"*len(confirm_str))
-            print(f"Target: {self._target_coords[0]} {self._target_coords[1]}")
-            print(f"Observer: {self._observer_coords[0]} {self._observer_coords[1]}")
-            print(f"Firing position: {self._firing_position_coords[0]} {self._firing_position_coords[1]}")
+            print(f"Target:\t\t{self._target_coords[0]} {self._target_coords[1]}")
+            print(f"Observer:\t{self._observer_coords[0]} {self._observer_coords[1]}")
+            print(f"Firing pos:\t{self._firing_position_coords[0]} {self._firing_position_coords[1]}")
             print()
 
-            while True:
-                action_str = input("'T', 'O' or 'F' to re-enter specific value.\n'Enter' to continue. 'E' to Exit:")
-                self._check_exit(action_str)
-                if action_str.upper() == "T":
-                    self._target_coords = self._get_coordinates("Target")
-                    break
-                if action_str.upper() == "O":
-                    self._observer_coords = self._get_coordinates("Observer")
-                    break
-                if action_str.upper() == "F":
-                    self._firing_position_coords = self._get_coordinates("Firing position")
-                    break
-                if action_str.upper() == "":
-                    break
+            action_str = input("Enter coordinates to fill next position.\n'T', 'O' or 'F' to change specific value.\n'Enter' to continue. 'E' to Exit: ")
+            self._check_exit(action_str)
+            if action_str.upper() == "T":
+                self._target_coords = self._get_coordinates("Target")
+                self._target_entered = True
+            if action_str.upper() == "O":
+                self._observer_coords = self._get_coordinates("Observer")
+                self._observer_entered = True
+            if action_str.upper() == "F":
+                self._firing_position_coords = self._get_coordinates("Firing position")
+                self._firing_position_entered = True
+
+            try:
+                int(action_str)
+                if not self._target_entered:
+                    self._target_coords = self._str_to_coords(action_str)
+                    self._target_entered = True
+                elif not self._observer_entered:
+                    self._observer_coords = self._str_to_coords(action_str)
+                    self._observer_entered = True
+                elif not self._firing_position_entered:
+                    self._firing_position_coords = self._str_to_coords(action_str)
+                    self._firing_position_entered = True
+            except ValueError:
+                pass
 
     def firing_values(self):
         pass
